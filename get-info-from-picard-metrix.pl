2c54c4743bd0f75598bed7c8bb27c7bda79fdd6d
hengtao <hengtaostar@163.com>
2018-08-15 10:42:58 +0800

    Update get-info-from-picard-metrix.pl

--- get-info-from-picard-metrix.pl
+++ get-info-from-picard-metrix.pl
@@ -25,33 +25,33 @@ sub info_from_picard_metrix{
     my ($inputdir, $outfile, $depCov) = @_;
     open(OUT, ">$outfile") or die "cannot write into this file:$!\n";
     print OUT "sampleID\tCov1X\tCov5X\tCov10X\n";
-    open(DEP, ">$depCov") or die "cannot write into this file:$!\n";
     my @metrixs = glob("$inputdir/*metrix");
     foreach my $metrix (@metrixs){
-    my ($base,$path,$type) = fileparse($metrix);
-    open IN, $metrix or die "cannot open this file:$!\n";
-    while(<IN>){
-        chomp;
-        if(/GENOME_TERRITORY/){
-            my $line = <IN>;
-            chomp $line;
-            my @array   = split(/\t/, $line);
-            my $genome  = &keep_decimal($array[0]);
-            my $meanCov = &keep_decimal($array[1]);
-            my $Cov1X   = &keep_decimal($array[12]);
-            my $Cov5X   = &keep_decimal($array[13]);
-            my $Cov10X  = &keep_decimal($array[14]);
-            print OUT "$base\t$Cov1X\t$Cov5X\t$Cov10X\n";
-        }elsif(/HISTOGRAM/){
-            while(<IN>){
-                print DEP "$_\n";
+        my ($base,$path,$type) = fileparse($metrix);
+        open(DEP, ">$inputdir/$base.$depCov") or die "cannot write into this file:$!\n";
+        open IN, $metrix or die "cannot open this file:$!\n";
+        while(<IN>){
+            chomp;
+            if(/GENOME_TERRITORY/){
+                my $line = <IN>;
+                chomp $line;
+                my @array   = split(/\t/, $line);
+                my $genome  = &keep_decimal($array[0]);
+                my $meanCov = &keep_decimal($array[1]);
+                my $Cov1X   = &keep_decimal($array[12]);
+                my $Cov5X   = &keep_decimal($array[13]);
+                my $Cov10X  = &keep_decimal($array[14]);
+                print OUT "$base\t$Cov1X\t$Cov5X\t$Cov10X\n";
+            }elsif(/HISTOGRAM/){
+                while(<IN>){
+                    print DEP "$_\n";
+                }
             }
         }
+        close IN;
+        close DEP;
     }
-    close IN;
-  }
-  close OUT;
-  close DEP;
+    close OUT;   
 }
 
 sub keep_decimal{
@@ -64,7 +64,7 @@ This script was used to evaluate the quality of the raw data\n
 Usage:\n
 perl runclean-01.pl     --inputdir      <inputdir contains metrix files, suffix of file is "metrix">
                         --outfile       <formatted output file>
-                        --depCov        <formatted file contains info:depth<tab>count>
+                        --depCov        <suffix of depth coverage file of all samples>
 EOF
 }
 
